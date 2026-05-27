06e10a75680c6052a9857bd65062ae04c087907c
zhaojay <zhaojay1211@gmail.com>
2026-05-27 22:56:13 +0800

    feat: 加入 sync

--- 1-10-sync/main.go
+++ 1-10-sync/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
 
 // sync.mutex 互斥锁
 // 同一时间，只有一个 goroutine 能够访问资源
@@ -21,6 +26,83 @@ import "fmt"
 // Done 减少等待的 goroutine 数量
 // Wait 等待所有 goroutine 执行完成
 
+type Inventory struct {
+	stock   int
+	rwMutex sync.RWMutex
+}
+
+func (v *Inventory) getStock() int {
+	v.rwMutex.RLock()
+	defer v.rwMutex.RUnlock()
+
+	return v.stock
+}
+
+func (v *Inventory) deductStock(quantity int) bool {
+	v.rwMutex.Lock()
+	defer v.rwMutex.Unlock()
+	if v.stock < quantity {
+		fmt.Printf("库存不足")
+		return false
+	}
+	time.Sleep(time.Millisecond * 100)
+	v.stock -= quantity
+	return true
+}
+
+type Counter struct {
+	value int64
+}
+
+func (c *Counter) Inc() {
+	atomic.AddInt64(&c.value, 1)
+}
+
+func (c *Counter) Dec() {
+	atomic.AddInt64(&c.value, -1)
+}
+
+func (c *Counter) Get() int64 {
+	return atomic.LoadInt64(&c.value)
+}
+
 func main() {
-	fmt.Println("Hello World")
+	// inventory := &Inventory{stock: 100}
+
+	var wg sync.WaitGroup
+
+	// for i := 0; i < 50; i++ {
+	// 	wg.Add(1)
+	// 	go func() {
+	// 		defer wg.Done()
+	// 		inventory.deductStock(1)
+	// 	}()
+	// }
+
+	// for i := 0; i < 50; i++ {
+	// 	time.Sleep(time.Millisecond * 100)
+	// 	fmt.Printf("剩余数量: %d \n", inventory.getStock())
+	// }
+	// wg.Wait()
+
+	// fmt.Printf("所有协程执行完毕，剩余数量: %d\n", inventory.getStock())
+
+	var counter Counter
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			counter.Inc()
+		}()
+	}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			counter.Dec()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Printf("所有协程执行完毕，点赞数量: %d\n", counter.Get())
 }
